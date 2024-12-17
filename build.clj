535a6e9dedc3ae18d258f5370aac36e015b0f376
Timothy Pratley <timothypratley@gmail.com>
2024-12-16 16:03:38 -0800

    move scm to map as recommended by write-pom docs

--- build.clj
+++ build.clj
@@ -18,55 +18,51 @@
 (defn clean [_]
   (b/delete {:path "target"}))
 
-(defn pom-template [version]
-  [[:description "`kindly-render` is a Clojure library for rendering kinds as markdown or html"]
-   [:url "https://scicloj.github.io/kindly-render"]
-   [:licenses
-    [:license
-     [:name "Eclipse Public License - v 2.0"]
-     [:url "https://www.eclipse.org/legal/epl-2.0/"]]]
-   [:scm
-    [:url "https://github.com/scicloj/kindly-render"]
-    [:connection "scm:git:https://github.com/scicloj/kindly-render.git"]
-    [:developerConnection "scm:git:ssh:git@github.com:scicloj/kindly-render.git"]
-    [:tag (str "v" version)]]])
-
 (defn jar [_]
-  (b/write-pom {:pom-data (pom-template version)
-                :class-dir class-dir
-                :lib lib
-                :version version
-                :basis basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     basis
+                :src-dirs  ["src"]
+                :pom-data  [[:description "`kindly-render` is a Clojure library for rendering kinds as markdown or html"]
+                            [:url "https://scicloj.github.io/kindly-render"]
+                            [:licenses
+                             [:license
+                              [:name "Eclipse Public License - v 2.0"]
+                              [:url "https://www.eclipse.org/legal/epl-2.0/"]]]]
+                :scm       {:url                 "https://github.com/scicloj/kindly-render"
+                            :connection          "scm:git:https://github.com/scicloj/kindly-render.git"
+                            :developerConnection "scm:git:ssh:git@github.com:scicloj/kindly-render.git"
+                            :tag                 (str "v" version)}})
+  (b/copy-dir {:src-dirs   ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
+          :jar-file  jar-file}))
 
 (defn install [_]
   (jar {})
-  (b/install {:basis basis
-              :lib lib
-              :version version
-              :jar-file jar-file
+  (b/install {:basis     basis
+              :lib       lib
+              :version   version
+              :jar-file  jar-file
               :class-dir class-dir}))
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs   ["src" "resources"]
                :target-dir class-dir})
-  (b/compile-clj {:basis basis
-                  :src-dirs ["src"]
+  (b/compile-clj {:basis     basis
+                  :src-dirs  ["src"]
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
-           :basis basis}))
+           :basis     basis}))
 
 (defn deploy [opts]
   (jar opts)
   ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
-    (merge {:installer :remote
-                       :artifact jar-file
-                       :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
-                    opts))
+   (merge {:installer :remote
+           :artifact  jar-file
+           :pom-file  (b/pom-path {:lib lib :class-dir class-dir})}
+          opts))
   opts)
